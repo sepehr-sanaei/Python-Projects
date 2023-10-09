807f0fa04299885e9f84e40d39c30e70f0d66731
sepehr-sanaei <147409951+sepehr-sanaei@users.noreply.github.com>
2023-10-09 17:42:37 +0330

    second commit

--- BTC_price.py
+++ BTC_price.py
@@ -4,22 +4,22 @@ import pyttsx3
 import time
 
 
-api = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+api = requests.get('#') # paste your coindesk api here
 api_read = json.loads(api.text)
 
 btc_old = api_read['bpi']['USD']['rate']
-btc_old_price = btc_old.replace(',', '')
+btc_old_price = btc_old.replace(',', '') # using this olad price to compare new and pld price
 
 
 
-while True:
-    api_2 = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+while True: # checking BTC price every 5 seconds
+    api_2 = requests.get("#") # paste your coindesk api here 
     api_read_2 = json.loads(api_2.text)
     btc = api_read_2['bpi']['USD']['rate']
     btc_price = btc.replace(',', '')
-    engine = pyttsx3.init()
+    engine = pyttsx3.init() # this is for saaying BTC price
     if btc_price != btc_old_price:
-        btc_new = float(btc_price)
+        btc_new = float(btc_price) # converting BTC price to float so we can Subtract it
         btc_past = float(btc_old_price)
         res = btc_new - btc_past
         res_round = round(res, 4)
